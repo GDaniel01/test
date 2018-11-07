9db82f71c26e9d093ceb17ef2b63e1661b0de314
Uwe Steinmann <steinm@debian.org>
2018-11-07 20:52:52 +0100

    better error handling if repository doesn't deliver an extension list

--- inc/inc.ClassExtensionMgr.php
+++ inc/inc.ClassExtensionMgr.php
@@ -465,16 +465,19 @@ class SeedDMS_Extension_Mgr {
 	public function updateExtensionList($version='', $force=false) { /* {{{ */
 		if($this->reposurl) {
 			if(!file_exists($this->cachedir."/".self::repos_list_file) || $force) {
-				$file = @file_get_contents($this->reposurl.($version ? '?seeddms_version='.$version : ''));
-				if(is_array($http_response_header)) {
-					$parts=explode(' ',$http_response_header[0]);
-					if(count($parts)>1) //HTTP/1.0 <code> <text>
-						if(intval($parts[1]) != 200) {
-							$this->errmsgs[] = 'Getting extension list returned http code ('.$parts[1].')';
-							return false;
-						}
+				if($file = @file_get_contents($this->reposurl.($version ? '?seeddms_version='.$version : ''))) {
+					if(is_array($http_response_header)) {
+						$parts=explode(' ',$http_response_header[0]);
+						if(count($parts)>1) //HTTP/1.0 <code> <text>
+							if(intval($parts[1]) != 200) {
+								$this->errmsgs[] = 'Getting extension list returned http code ('.$parts[1].')';
+								return false;
+							}
+					}
+					file_put_contents($this->cachedir."/".self::repos_list_file, $file);
+				} else {
+					return false;
 				}
-				file_put_contents($this->cachedir."/".self::repos_list_file, $file);
 			}
 			return true;
 		} else {
